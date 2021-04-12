3941cac12031592afdf4bfef792955992a2b9bfd
Alexander Zakharov <alzak@envisage.no>
2021-04-12 16:17:10 +0200

    chore: convert field-input component to TypeScript

---
+++ src/components/fields/field-input/field-input.component.tsx
@@ -0,0 +1,41 @@
+import React, { memo, FC } from 'react';
+import { compose } from 'redux';
+import type { WrappedFieldProps } from 'redux-form';
+import cx from 'classnames';
+
+interface ExternalProps extends WrappedFieldProps {
+  label?: string;
+  showLabel?: boolean;
+  type?: string;
+  language?: string;
+  isOnlyOneSelectedLanguage?: boolean;
+}
+
+interface Props extends ExternalProps {}
+
+const InputField: FC<Props> = ({
+  input,
+  label,
+  type,
+  meta: { touched, error, warning },
+  showLabel,
+  language,
+  isOnlyOneSelectedLanguage
+}) => (
+  <div className={cx('pl-2', { 'multilingual-field': !!language })}>
+    <div className='d-flex align-items-center'>
+      <label className='fdk-form-label w-100' htmlFor={input.name}>
+        {showLabel && label && <span className='mb-2 d-block'>{label}</span>}
+        {!!language && !isOnlyOneSelectedLanguage && (
+          <span className='language-indicator'>{language}</span>
+        )}
+        <input {...input} type={type} className='form-control' />
+      </label>
+    </div>
+    {touched &&
+      ((error && <div className='alert alert-danger mt-3'>{error}</div>) ||
+        (warning && <div className='alert alert-warning mt-3'>{warning}</div>))}
+  </div>
+);
+
+export default compose<FC<ExternalProps>>(memo)(InputField);
