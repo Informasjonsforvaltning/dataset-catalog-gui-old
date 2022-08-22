68aa406889f6dcc0f322e8b1ccc17868072e2f90
terjesyl <108797958+terjesyl@users.noreply.github.com>
2022-08-22 13:28:23 +0200

    refactor: delete old code

--- src/components/form-publish/form-publish-configurer.js
+++
@@ -1,7 +0,0 @@
-import { reduxForm } from 'redux-form';
-import { asyncValidateDatasetInvokePatch } from '../dataset-registration-form/formsLib/asyncValidateDatasetInvokePatch';
-
-export const formPublishConfigurer = reduxForm({
-  form: 'datasetPublish',
-  asyncValidate: asyncValidateDatasetInvokePatch
-});
--- src/types/common.d.ts
+++
@@ -1,3 +0,0 @@
-export type Actions<T extends { [key: string]: (...args: any) => any }> = {
-  [K in keyof T]: ReturnType<T[K]>;
-}[keyof T];
