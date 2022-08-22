92e11e958fe962cf1a0bb682d7ec65f81fa1a759
terjesyl <108797958+terjesyl@users.noreply.github.com>
2022-08-22 14:03:21 +0200

    refactor: hello new-dataset-catalog-gui

---
+++ src/components/app/index.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from "react";
+import Main from "../pages/main";
+
+const App: FC = () => <Main />;
+
+export default App;
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Footer: FC = () => (
+  <></>
+);
+
+export default Footer;
