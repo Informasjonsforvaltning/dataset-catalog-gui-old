73fb0516090d101835a575113eba55f5e4940cd4
Pooria Mehregan <pooriamehregan@gmail.com>
2022-09-16 10:35:35 +0200

    feat: add datasetpage, with routing

---
+++ src/components/pages/datasets-page/index.tsx
@@ -0,0 +1,57 @@
+import React, { memo, FC, useState } from 'react';
+import { Link, Outlet, useParams } from 'react-router-dom';
+import { compose } from 'redux';
+import Breadcrumbs, { Breadcrumb } from '@fellesdatakatalog/breadcrumbs';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import SearchIcon from '@mui/icons-material/Search';
+
+import env from '../../../utils/constants/env';
+import { withAuth } from '../../../utils/authentication/auth-provider';
+import SC from './styled';
+import { localization } from '../../../utils/language/localization';
+// import { routes } from '../../../router/routes';
+
+const DatasetsPage: FC = () => {
+  const { FDK_REGISTRATION_BASE_URI } = env;
+  const { catalogId } = useParams();
+  const [query, setQuery] = useState(localization.searchForDatasetDescription);
+
+  return (
+    <>
+      <Breadcrumbs as={SC.Breadcrumbs}>
+        <Breadcrumb>
+          <a href={FDK_REGISTRATION_BASE_URI}>{localization.allCatalogs}</a>
+        </Breadcrumb>
+        <Breadcrumb active>
+          <Link to={`/dataset-catalogs/${catalogId}`}>{localization.catalogType}</Link>
+        </Breadcrumb>
+      </Breadcrumbs>
+      <SC.Page>
+        <SC.Title>{localization.catalogType}</SC.Title>
+        <SC.SubTitle>{catalogId}</SC.SubTitle>
+        <SC.ListActions>
+          <SC.CreateButton
+            type='button'
+            disabled={!query || query === localization.searchForDatasetDescription}
+            onClick={() => {}}
+          >
+            <AddCircleOutlineIcon />
+            {localization.addNewDatasettDescriptionBtn}
+          </SC.CreateButton>
+          <SC.SearchBox role='search'>
+            <SC.SearchField
+              type='text'
+              placeholder={localization.searchForDatasetDescription}
+              value={query && query !== localization.searchForDatasetDescription ? query : ''}
+              onChange={event => setQuery(event.target.value)}
+            />
+            <SearchIcon />
+          </SC.SearchBox>
+        </SC.ListActions>
+      </SC.Page>
+      <Outlet />
+    </>
+  );
+};
+
+export default compose<FC>(memo, withAuth)(DatasetsPage);
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,13 @@
 import React, { FC } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from '../components/main';
-import MainPage from '../components/pages/main-page';
+import DatasetsPage from '../components/pages/datasets-page';
 
 const Router: FC = () => (
   <BrowserRouter>
     <Routes>
       <Route path={'/dataset-catalogs'} element={<Main />}>
-        <Route path=':id' element={<MainPage />} />
+        <Route path=':id' element={<DatasetsPage />} />
       </Route>
     </Routes>
   </BrowserRouter>
