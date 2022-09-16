0aebac0b46521547969fde52fd89a0efc9a25d90
Pooria Mehregan <pooriamehregan@gmail.com>
2022-09-16 09:41:08 +0200

    feat: add translation

---
+++ src/utils/language/nb.ts
@@ -0,0 +1,6 @@
+export const nb = {
+  allCatalogs: 'Alle kataloger',
+  addNewDatasettDescriptionBtn: 'Legg til ny datasettbeskrivelse',
+  searchForDatasetDescription: 'Søk etter datasettbeskrivelse',
+  catalogType: 'Datasettkatalog',
+};
