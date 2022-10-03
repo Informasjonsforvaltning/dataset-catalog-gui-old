28bb55a738c9880a15da170bf69d1d2c1ab721fa
Pooria Mehregan <pooriamehregan@gmail.com>
2022-10-03 13:54:04 +0200

    feat: add icon

---
+++ src/components/icon/styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import SvgIcon from '@fellesdatakatalog/icons';
+
+const Icon = styled(SvgIcon)`
+  width: 22px;
+  height: 22px;
+
+  & * {
+    stroke: #fff;
+  }
+`;
+
+export default { Icon };
