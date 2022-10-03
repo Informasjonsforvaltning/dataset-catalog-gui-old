0e20c46a135dfe8e7dabd0675a8b07a1a3e63070
Pooria Mehregan <pooriamehregan@gmail.com>
2022-10-03 13:56:54 +0200

    refactor: datasets page

--- src/components/pages/datasets-page/styled.ts
+++ src/components/pages/datasets-page/styled.ts
@@ -1,22 +1,58 @@
 import styled from 'styled-components';
-import { theme } from '@fellesdatakatalog/theme';
+import Button from '../../button';
+
+const Page = styled.section``;
 
 const Title = styled.h1`
-  font-size: ${theme.fontSize('FS40')};
-  font-weight: ${theme.fontWeight('FW700')};
+  width: 1110px;
+  height: 58px;
+  font-family: 'Heebo', sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 47px;
+  line-height: 58px;
+  color: #2d3741;
 `;
 
 const SubTitle = styled.h2`
-  font-size: ${theme.fontSize('FS20')};
-  font-weight: ${theme.fontWeight('FW400')};
+  width: 1110px;
+  height: 30px;
+  font-family: 'Heebo';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 19px;
+  line-height: 30px;
+  color: #2d3741;
 `;
 
-const Page = styled.section`
-  margin-top: ${theme.spacing('S24')};
+const AddDiv = styled.div`
+  display: flex;
+  margin: 30px 0 20px 0;
+
+  span:first-child {
+    margin-right: 10px;
+  }
+`;
+
+const HostButton = styled(Button)`
+  background-color: #d5e1f2;
+  color: #335380;
+  min-width: 220px;
+  margin-left: 10px;
+
+  & button {
+    color: #335380;
+  }
+
+  & * {
+    stroke: #000;
+  }
 `;
 
 export default {
+  AddDiv,
   Title,
   SubTitle,
   Page,
+  HostButton,
 };
