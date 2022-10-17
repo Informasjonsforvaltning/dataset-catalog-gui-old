48865f2d077ae629575c9426703bdbe6000b1d8d
Pooria Mehregan <pooriamehregan@gmail.com>
2022-10-17 13:53:18 +0200

    refactore: remove auth wrapper

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,29 @@
 import React, { FC, StrictMode } from 'react';
 import ThemeProvider from '@fellesdatakatalog/theme';
+import { CookiesProvider } from 'react-cookie';
 
 import AppContext from '../context/main-context';
 import Router from '../router';
-import AuthWrapper from '../utils/authentication/auth-wrapper';
 import SC from './styled';
+import AuthProvider from '../utils/authentication/auth-provider';
+import DatasetsContext from '../context/datasets-context';
 
 const App: FC = () => {
   return (
     <StrictMode>
-      <ThemeProvider>
-        <SC.App>
-          <AuthWrapper>
-            <AppContext>
-              <Router />
-            </AppContext>
-          </AuthWrapper>
-        </SC.App>
-      </ThemeProvider>
+      <CookiesProvider>
+        <AuthProvider>
+          <ThemeProvider>
+            <SC.App>
+              <AppContext>
+                <DatasetsContext>
+                  <Router />
+                </DatasetsContext>
+              </AppContext>
+            </SC.App>
+          </ThemeProvider>
+        </AuthProvider>
+      </CookiesProvider>
     </StrictMode>
   );
 };
--- src/app/styled.ts
+++ src/app/styled.ts
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
 import { theme, Colour } from '@fellesdatakatalog/theme';
 
-const App = styled.body`
+const App = styled.div`
+  display: flex;
+  flex-direction: column;
   background-color: ${theme.colour(Colour.GREEN, 'G15')};
+  overflow-x: hidden;
+  height: 100vh;
 `;
 
 export default { App };
